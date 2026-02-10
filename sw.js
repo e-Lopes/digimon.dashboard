6ca00fdfe018325fb27b75223dfb475789d16b40
Eduardo Lopes <eduardo.lamy.lopes@gmail.com>
2026-02-10 18:49:02 -0300

    novo titulo

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'digimon-tcg-v1';
+// 1. Mudei o nome para digistats e a versão para v2
+const CACHE_NAME = 'digistats-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -7,14 +8,32 @@ const ASSETS = [
   './manifest.json'
 ];
 
-// Instala o Service Worker e armazena os arquivos básicos
+// Instala o Service Worker
 self.addEventListener('install', (event) => {
+  // O skipWaiting faz o novo SW assumir o controle imediatamente
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Responde com o cache quando estiver offline
+// 2. LOGICA NOVA: Deleta o cache antigo (v1) para não dar conflito
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Limpando cache antigo...');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Responde com o cache
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
